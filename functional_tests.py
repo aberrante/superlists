ddaf245d14e02e2bbb9223ce00b62b888308d397
aberrante <aberrante@gmail.com>
2017-05-26 09:19:05 -0400

    Redirect after POST, and show all items in template

--- functional_tests.py
+++ functional_tests.py
@@ -38,6 +38,8 @@ class NewVisitorTest(unittest.TestCase):
 		#"1: Buy peacock feathers" as an item in a to-do list
 		inputbox.send_keys(Keys.ENTER)
 		
+		import time
+		time.sleep(1)
 		
 		#There is still a text box inviting her to add another item.
 		#She enteres "Use peacock feathers to make a fly"
@@ -45,6 +47,9 @@ class NewVisitorTest(unittest.TestCase):
 		inputbox = self.browser.find_element_by_id('id_new_item')
 		inputbox.send_keys('Use peacock feathers to make a fly')
 		inputbox.send_keys(Keys.ENTER)
+		
+		time.sleep(1)
+		
 		
 		#The page updates again, and now shows both items on her list
 		self.check_for_row_in_list_table('1: Buy peacock feathers')
