851d8c86f568d29f3ee66555c5ca0f1e770130c6
aberrante <aberrante@gmail.com>
2017-06-07 12:44:27 -0400

    Updated functional_tests to align with 2nd edition

--- functional_tests/tests.py
+++ functional_tests/tests.py
@@ -1,11 +1,14 @@
 from django.contrib.staticfiles.testing import StaticLiveServerTestCase
 from selenium import webdriver
+from selenium.common.exceptions import WebDriverException
 from selenium.webdriver.common.keys import Keys
+from unittest import skip
 import time
 import sys
 
+MAX_WAIT = 10
+
 class NewVisitorTest(StaticLiveServerTestCase):
-	
 	@classmethod
 	def setUpClass(cls):
 		for arg in sys.argv:
@@ -19,21 +22,28 @@ class NewVisitorTest(StaticLiveServerTestCase):
 	def tearDownClass(cls):
 		if cls.server_url == cls.live_server_url:
 			super().tearDownClass()
-	
-	
+		
 	def setUp(self):
 		self.browser = webdriver.Firefox()
 		self.browser.implicitly_wait(3)
 		
 	def tearDown(self):
 		self.browser.quit()
-	
-	def check_for_row_in_list_table(self, row_text):
-		table = self.browser.find_element_by_id('id_list_table')
-		rows = table.find_elements_by_tag_name('tr')
-		self.assertIn(row_text, [row.text for row in rows] )
 
-	def test_can_start_a_list_and_retrieve_it_later(self):
+	def wait_for_row_in_list_table(self, row_text):
+		start_time = time.time()
+		while True:
+			try:
+				table = self.browser.find_element_by_id('id_list_table')
+				rows = table.find_elements_by_tag_name('tr')
+				self.assertIn(row_text, [row.text for row in rows] )
+				return
+			except (AssertionError, WebDriverException) as e:
+				if time.time() - start_time> MAX_WAIT:
+					raise e
+				time.sleep(0.5)
+	
+	def test_can_start_a_list_for_one_user(self):
 		#Edith has heard about a cool new online to-do app. She goes
 		#to check out its homepage
 		self.browser.get(self.server_url)
@@ -55,11 +65,11 @@ class NewVisitorTest(StaticLiveServerTestCase):
 		#"1: Buy peacock feathers" as an item in a to-do list
 		inputbox.send_keys(Keys.ENTER)
 		
-		time.sleep(1)
-		
+		# time.sleep(1)
+		self.wait_for_row_in_list_table('1: Buy peacock feathers')
 		edith_list_url = self.browser.current_url
 		self.assertRegex(edith_list_url, '/lists/.+')
-		self.check_for_row_in_list_table('1: Buy peacock feathers')
+		
 		
 		#There is still a text box inviting her to add another item.
 		#She enteres "Use peacock feathers to make a fly"
@@ -68,11 +78,24 @@ class NewVisitorTest(StaticLiveServerTestCase):
 		inputbox.send_keys('Use peacock feathers to make a fly')
 		inputbox.send_keys(Keys.ENTER)
 		
-		time.sleep(1)
-		
 		#The page updates again, and now shows both items on her list
-		self.check_for_row_in_list_table('2: Use peacock feathers to make a fly')
-		self.check_for_row_in_list_table('1: Buy peacock feathers')
+		self.wait_for_row_in_list_table('2: Use peacock feathers to make a fly')
+		self.wait_for_row_in_list_table('1: Buy peacock feathers')
+		
+		#Satisfied, she goes back to sleep
+	
+	def test_multiple_users_can_start_lists_at_different_urls(self):	
+		#Edith stat a new todo list
+		
+		self.browser.get(self.live_server_url)
+		inputbox = self.browser.find_element_by_id('id_new_item')
+		inputbox.send_keys('Buy peacock feathers')
+		inputbox.send_keys(Keys.ENTER)
+		self.wait_for_row_in_list_table('1: Buy peacock feathers')
+		
+		#She notices that her list has a unique URL
+		edith_list_url = self.browser.current_url
+		self.assertRegex(edith_list_url, '/lists/.+')
 		
 		#Now a new user, Francis, comes along to the site.
 		##We use a new browser session to make sure that no information
@@ -90,8 +113,9 @@ class NewVisitorTest(StaticLiveServerTestCase):
 		inputbox = self.browser.find_element_by_id('id_new_item')
 		inputbox.send_keys('Buy milk')
 		inputbox.send_keys(Keys.ENTER)
+		self.wait_for_row_in_list_table('1: Buy milk')
 		
-		time.sleep(1)
+		# time.sleep(1)
 		
 		#Francis gets his own unique URL
 		francis_list_url = self.browser.current_url
@@ -103,14 +127,7 @@ class NewVisitorTest(StaticLiveServerTestCase):
 		self.assertNotIn('Buy peacock feathers', page_text)
 		self.assertIn('Buy milk', page_text)
 		
-		#Edith wonders whether the site will remember her list. 
-		#Then she sees that the site has generated a unique URL for her 
-		#-- there is some explanatory text to that effect.
-
-		#She visits that URL - her to-do list is still there
-
 		#Satisfied, they both go back to sleep.
-		# self.fail('Finish the test!')
 		
 	def test_layout_and_styling(self):
 		#Edith goes to the home page
@@ -125,5 +142,24 @@ class NewVisitorTest(StaticLiveServerTestCase):
 		inputbox.send_keys('testing')
 		inputbox.send_keys(Keys.ENTER)
 		time.sleep(1)
+		
 		inputbox = self.browser.find_element_by_id('id_new_item')
 		self.assertAlmostEqual(inputbox.location['x'] + inputbox.size['width'] / 2, 512, delta = 10)
+	
+	@skip
+	def test_cannot_add_emtpy_list_items(self):
+		#Edith goes to the home page and accidentally tries to submit
+		#an empty list item. She hits Enter on the empty input box
+		
+		#The home page refreshes, and there is an error message saying 
+		#that list items cannot be blank
+		
+		#She tries again with some text for the item, which now works
+		
+		#Perversely, she now decide to submit a second blank list item
+		
+		#She receives a similar warning on the list page
+		
+		#And she can correct it by filling some text in 
+		
+		self.fail('write me!')
