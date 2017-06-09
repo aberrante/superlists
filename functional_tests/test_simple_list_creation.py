57365221547016f55847ea2cc8463db7c789e894
aberrante <aberrante@gmail.com>
2017-06-09 16:57:21 -0400

    Moved Fts into their own inidividual files

--- functional_tests/test_simple_list_creation.py
+++ functional_tests/test_simple_list_creation.py
@@ -1,52 +1,14 @@
-from django.contrib.staticfiles.testing import StaticLiveServerTestCase
+from .base import FunctionalTest
 from selenium import webdriver
-from selenium.common.exceptions import WebDriverException
 from selenium.webdriver.common.keys import Keys
-from unittest import skip
 import time
-import sys
 
-MAX_WAIT = 10
-
-class NewVisitorTest(StaticLiveServerTestCase):
-	@classmethod
-	def setUpClass(cls):
-		for arg in sys.argv:
-			if 'liveserver' in arg:
-				cls.server_url ='http://' + arg.split('=')[1]
-				return
-		super().setUpClass()
-		cls.server_url = cls.live_server_url
-	
-	@classmethod
-	def tearDownClass(cls):
-		if cls.server_url == cls.live_server_url:
-			super().tearDownClass()
+class NewVisitorTest(FunctionalTest):
 		
-	def setUp(self):
-		self.browser = webdriver.Firefox()
-		self.browser.implicitly_wait(3)
-		
-	def tearDown(self):
-		self.browser.quit()
-
-	def wait_for_row_in_list_table(self, row_text):
-		start_time = time.time()
-		while True:
-			try:
-				table = self.browser.find_element_by_id('id_list_table')
-				rows = table.find_elements_by_tag_name('tr')
-				self.assertIn(row_text, [row.text for row in rows] )
-				return
-			except (AssertionError, WebDriverException) as e:
-				if time.time() - start_time> MAX_WAIT:
-					raise e
-				time.sleep(0.5)
-	
 	def test_can_start_a_list_for_one_user(self):
 		#Edith has heard about a cool new online to-do app. She goes
 		#to check out its homepage
-		self.browser.get(self.server_url)
+		self.browser.get(self.live_server_url)
 
 		#She notices the page title and header mention to-do lists
 		self.assertIn('To-Do', self.browser.title)
@@ -104,7 +66,7 @@ class NewVisitorTest(StaticLiveServerTestCase):
 		self.browser = webdriver.Firefox()
 		
 		#Francis visits the home page. There is no sign of Edith's list
-		self.browser.get(self.server_url)
+		self.browser.get(self.live_server_url)
 		page_text = self.browser.find_element_by_tag_name('body').text
 		self.assertNotIn('Buy peacock feathers', page_text)
 		self.assertNotIn('make a fly', page_text)
@@ -128,38 +90,4 @@ class NewVisitorTest(StaticLiveServerTestCase):
 		self.assertIn('Buy milk', page_text)
 		
 		#Satisfied, they both go back to sleep.
-		
-	def test_layout_and_styling(self):
-		#Edith goes to the home page
-		self.browser.get(self.server_url)
-		self.browser.set_window_size(1024, 768)
-		
-		#She notices the input ox is nicely centered
-		inputbox = self.browser.find_element_by_id('id_new_item')
-		self.assertAlmostEqual(inputbox.location['x'] + inputbox.size['width'] / 2, 512, delta = 10)
-		
-		#She starts a new list and sees the input is nicely centered there too
-		inputbox.send_keys('testing')
-		inputbox.send_keys(Keys.ENTER)
-		time.sleep(1)
-		
-		inputbox = self.browser.find_element_by_id('id_new_item')
-		self.assertAlmostEqual(inputbox.location['x'] + inputbox.size['width'] / 2, 512, delta = 10)
-	
-	@skip
-	def test_cannot_add_emtpy_list_items(self):
-		#Edith goes to the home page and accidentally tries to submit
-		#an empty list item. She hits Enter on the empty input box
-		
-		#The home page refreshes, and there is an error message saying 
-		#that list items cannot be blank
-		
-		#She tries again with some text for the item, which now works
-		
-		#Perversely, she now decide to submit a second blank list item
-		
-		#She receives a similar warning on the list page
-		
-		#And she can correct it by filling some text in 
-		
-		self.fail('write me!')
+
