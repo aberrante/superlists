a03569a0266aa50a62a68a9b164b27049d56fc33
aberrante <aberrante@gmail.com>
2017-06-15 15:50:33 -0400

    save method moved to form

--- lists/tests/test_form.py
+++ lists/tests/test_form.py
@@ -1,4 +1,5 @@
 from django.test import TestCase
+from lists.models import List, Item
 from lists.forms import EMPTY_ITEM_ERROR, ItemForm
 
 class ItemFormTest(TestCase):
@@ -11,3 +12,11 @@ class ItemFormTest(TestCase):
 		form = ItemForm({'text': ''})
 		self.assertFalse(form.is_valid())
 		self.assertEqual(form.errors['text'], [EMPTY_ITEM_ERROR])
+	
+	def test_form_save_handles_saving_to_a_list(self):
+		list_ = List.objects.create()
+		form = ItemForm(data = {'text': 'save me'})
+		new_item = form.save(for_list = list_)
+		self.assertEqual(new_item, Item.objects.first())
+		self.assertEqual(new_item.text, 'save me')
+		self.assertEqual(new_item.list, list_)
