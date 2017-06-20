cd1bb31dc99ba819410cab1fdcd2f7cf847dc6e0
aberrante <aberrante@gmail.com>
2017-06-20 12:54:49 -0400

    Custome passwordless auth backend + custom user model

--- superlists/settings.py
+++ superlists/settings.py
@@ -38,7 +38,13 @@ INSTALLED_APPS = (
     'django.contrib.messages',
     'django.contrib.staticfiles',
 	'lists',
+	'accounts',
 )
+
+AUTH_USER_MODEL = 'accounts.User'
+AUTHENTICATION_BACKENDS = [
+	'accounts.authentication.PasswordlessAuthenticationBackend',
+]
 
 MIDDLEWARE_CLASSES = (
     'django.contrib.sessions.middleware.SessionMiddleware',
@@ -102,3 +108,29 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.abspath(os.path.join(BASE_DIR, '../static'))
+
+#Logging
+
+LOGGING = {
+	'version': 1, 
+	'disable_existing_loggers': False,
+	'handlers': {
+		'console': {
+			'level': 'DEBUG',
+			'class': 'logging.StreamHandler',
+		},
+	},
+	'loggers': {
+		'django': {
+			'handlers': ['console'],
+		},
+	},
+	'root': {'level': 'INFO'},
+}
+
+#email settings
+EMAIL_USE_TLS = True
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_HOST_USER = 'oddityhub@gmail.com'
+EMAIL_PORT = 587
+EMAIL_HOST_PASSWORD = os.environ.get('EMAIL_PASSWORD')
